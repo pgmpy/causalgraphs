0b78985fe7843796d760f1ec4196597637ac5c94
Mohammed Razak <mohammedrazak2001@gmail.com>
2025-06-20 00:46:49 +0530

    add python sdk

---
+++ python/pgmpy_rust/test.py
@@ -0,0 +1,9 @@
+from pgmpy_rust import RustDAG
+
+# Drop-in replacement for performance-critical operations
+dag = RustDAG([('A', 'B'), ('B', 'C'), ('D', 'C')])
+
+# These operations are now much faster
+ancestors = dag._get_ancestors_of(['C'])
+separator = dag.minimal_dseparator('A', 'D')
+is_connected = dag.is_dconnected('A', 'D', ['B'])
